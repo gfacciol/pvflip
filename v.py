f5a4c33c82245d4d7cb3ab642c1f096026881f3d
Gabriele Facciolo <gfacciol@gmail.com>
2014-07-29 11:17:13 +0200

    licence stuff

--- v.py
+++ v.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 # Copyright 2013, Gabriele Facciolo <facciolo@cmla.ens-cachan.fr>
+#
+# This file is part of Pvflip.
+# 
+# Pvflip is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Pvflip is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with Pvflip.  If not, see <http://www.gnu.org/licenses/>.
+
 ############################################################################
 #
 # v03 : faster image load with piio.read_buffer avoids passing trough numpy
