c4bb70f65f04585fd9a0fdba35bb18f4702d18a6
Gabriele Facciolo <gfacciol@gmail.com>
2016-07-29 00:15:34 +0200

    fix vandalism

--- v.py
+++ v.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # Copyright 2013, Gabriele Facciolo <facciolo@cmla.ens-cachan.fr>
 ############################################################################
 #
